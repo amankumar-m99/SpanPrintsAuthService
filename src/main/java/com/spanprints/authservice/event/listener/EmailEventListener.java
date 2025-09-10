35af9e2e143e8b81eee094723fb44a4d72823069
Aman Kumar <amankumar.m99@gmail.com>
2025-09-10 22:35:57 +0530

    added listener for email invokation

---
+++ src/main/java/com/spanprints/authservice/event/listener/EmailEventListener.java
@@ -0,0 +1,23 @@
+package com.spanprints.authservice.event.listener;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.spanprints.authservice.event.AccountRegisteredEvent;
+import com.spanprints.authservice.service.MailService;
+
+@Component
+public class EmailEventListener {
+
+	private MailService mailService;
+
+	public EmailEventListener(MailService mailService) {
+		this.mailService = mailService;
+	}
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleAccountRegisteredEvent(AccountRegisteredEvent event) {
+		mailService.sendVerificationMail(event.getEmail(), event.getTokenResponseDto());
+	}
+}
