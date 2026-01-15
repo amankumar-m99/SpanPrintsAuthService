d01423511cd4536bc9dfcc500071d336932a9987
Aman Kumar <amankumar.m99@gmail.com>
2026-01-16 02:23:10 +0530

    removed non-required code

--- src/main/java/com/spanprints/authservice/entity/Expense.java
+++ src/main/java/com/spanprints/authservice/entity/Expense.java
@@ -5,10 +5,8 @@ import java.time.Instant;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.spanprints.authservice.enums.ExpenseType;
 
-import io.jsonwebtoken.lang.Collections;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -39,26 +37,8 @@ public class Expense extends AuditableBaseEntity {
 	@JsonIgnore
 	private Account account;
 
-	@JsonProperty("createBy")
-	public String getAddedBy() {
-		return account != null ? account.getUsername() : null;
-	}
-
-	@JsonProperty("createdById")
-	public Long getAccountId() {
-		return account != null ? account.getId() : null;
-	}
-
 	@OneToMany(mappedBy = "expense", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 	@JsonIgnore
 	private List<Ledger> ledgers;
-
-	@JsonProperty("ledgerIds")
-	public List<Long> getLedgerIds() {
-		if (ledgers == null) {
-			return Collections.emptyList();
-		}
-		return ledgers.stream().map(Ledger::getId).toList();
-	}
 
 }
