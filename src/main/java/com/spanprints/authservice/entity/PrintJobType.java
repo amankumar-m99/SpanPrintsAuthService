26ae6cd0ec17628b7b5a1d06dd0697940d9473ad
Aman Kumar <amankumar.m99@gmail.com>
2026-07-13 01:51:12 +0530

    column rename
    Signed-off-by: Aman Kumar <amankumar.m99@gmail.com>

--- src/main/java/com/spanprints/authservice/entity/PrintJobType.java
+++ src/main/java/com/spanprints/authservice/entity/PrintJobType.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Builder
 public class PrintJobType extends AuditableBaseEntity {
 
+	@Column(name = "jobId")
 	private String code;
 	private String name;
 	private String description;
